3517f7c65daa5a6cf8367d8b87cd95e65bc14061
oshaq naimat <oshaqnaimat15@gmail.com>
2025-10-28 19:24:26 +0500

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <div className="container my-6 rounded-md mx-auto shadow shadow-black p-3 w-[90%] md:w-1/2 lg:w-1/3">
+    <form >
+      <label htmlFor="" className='font-semibold '>Language</label>
+      <input type="text" placeholder='e.g Javascript' className='w-full outline-0 border-1
+       rounded-md p-1 border-red-500' />
+       <label htmlFor="" className='font-semibold '>Description</label>
+       <textarea name="" placeholder='About Language .....' className='w-full p-2 outline-0 border-1 rounded-md border-green-500'></textarea>
+       <label htmlFor="" className='font-semibold '>Type</label>
+       <select name="" id="" className='w-full p-1 outline-0 border-1 rounded-md border-blue-500'>
+        <option value="Assembly language">Assembly language</option>
+        <option value="Low Level language">Low Level language</option>
+        <option value="High Level language">High Level language</option>
+       </select>
+    </form>
+
+    </div>
+    </>
   )
 }
 
