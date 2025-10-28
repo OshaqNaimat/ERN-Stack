49263e2d7423d951571652fdce2199b9b7970538
oshaq naimat <oshaqnaimat15@gmail.com>
2025-10-28 22:59:44 +0500

    Post API

--- src/Add.jsx
+++ src/Add.jsx
@@ -5,27 +5,45 @@ import { useState } from 'react'
 const Add = ({getData}) => {
 
 
-    const [language, setLanguage] = useState('')
-    const [description, setDescription] = useState('')
-    const [type, setType] = useState('')
+    const [inputs, setInputs] = useState({
+        language:"",
+        description:"",
+        type:''
+    })
+
+    const {language,description,type} = inputs
  
+    const handleChange = (e)=>{
+          setInputs({
+            ...inputs,
+            [e.target.name] : e.target.value
+          })
+    }
+
+    const handleLanguage = async (e)=>{
+        e.preventDefault()
+        let response = await axios.post('http://localhost:5555/add-language',{
+            language,description,type
+        })
+        console.log(response)
+    }
 
   return (
     <>
     <div className="container my-6 rounded-md mx-auto shadow shadow-black p-3 w-[90%] md:w-1/2 lg:w-1/3">
     <form >
       <label htmlFor="" className='font-semibold '>Language</label>
-      <input  type="text"  placeholder='e.g Javascript' className='w-full outline-0 border-1
+      <input value={language} onChange={handleChange} name='language' type="text"  placeholder='e.g Javascript' className='w-full outline-0 border-1
        rounded-md p-1 border-red-500' />
        <label htmlFor="" className='font-semibold '>Description</label>
-       <textarea  placeholder='About Language .....' className='w-full p-2 outline-0 border-1 rounded-md border-green-500'></textarea>
+       <textarea value={description} onChange={handleChange} name='description'  placeholder='About Language .....' className='w-full p-2 outline-0 border-1 rounded-md border-green-500'></textarea>
        <label htmlFor="" className='font-semibold '>Type</label>
-       <select  id=""  className='w-full p-1 outline-0 border-1 rounded-md border-blue-500'>
+       <select value={type} onChange={handleChange} id="" name='type'  className='w-full p-1 outline-0 border-1 rounded-md border-blue-500'>
         <option value="Assembly language">Assembly language</option>
         <option value="Low Level language">Low Level language</option>
         <option value="High Level language">High Level language</option>
        </select>
-       <button className='my-2 w-full bg-green-500 rounded-md text-white p-1 cursor-pointer hover:bg-green-600 active:scale-90 duration-100'>Add Language</button>
+       <button onClick={handleLanguage} className='my-2 w-full bg-green-500 rounded-md text-white p-1 cursor-pointer hover:bg-green-600 active:scale-90 duration-100'>Add Language</button>
     </form>
 
     </div>
